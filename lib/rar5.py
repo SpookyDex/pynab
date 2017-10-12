e64b55157b9f7ef51f36ee0f3c4b296437da84e8
Neil Betham <nbetham@me.com>
2017-10-11 20:26:53 -0400

    Commiting work to move to another host

--- lib/rar5.py
+++ lib/rar5.py
@@ -7,26 +7,75 @@ import sys
 import time
 import zlib
 import re
+import io
 
 # The SFX has a current limit of 1MB
 MAX_SIGNATURE_SCAN = (1000 ** 2)
 
 # Archive Versions
 RAR_FORMATS = {
-    0: '1.5',
+    0: '4.0',
     1: '5.0',
 }
+
+# Header Types
+HEADER_TYPES = {
+    0: {
+        114: 'mark',
+        115: 'main',
+        116: 'file',
+        117: 'old-comment',
+        118: 'old-auth-info',
+        119: 'old-sub-block',
+        120: 'old-recovery-record',
+        121: 'old-sign',
+        122: 'service',
+        123: 'end'
+    },
+    1: {
+        0: 'mark',
+        1: 'main',
+        2: 'file',
+        3: 'service',
+        4: 'encryption',
+        5: 'end'
+    },
+}
+
+# Header Flag Masks
+HEADER_FLAG_MASKS = {
+    'extra_area': 0x0001,
+    'data_area': 0x0002,
+    'skip_unknown': 0x0004,
+    'data_continues_prev': 0x0008,
+    'data_continues_next': 0x0010,
+    'depends_on_prev': 0x0020,
+    'preserve_child': 0x0040,
+}
+
 
 class BadRarFile(RuntimeError):
     pass
 
 
-def read_byte(stream):
+class CorruptedRarFile(RuntimeError):
+    pass
+
+
+def read_bytes(stream, count):
+    return ''.join(range(0, count).map(lambda _: struct.unpack_from('<c', stream)))
+
+
+def read_u8(stream):
     return struct.unpack_from('<B', stream)
 
 
-def read_long(stream):
+def read_u32(stream):
     return struct.unpack_from('<L', stream)
+
+
+def read_u64(stream):
+    return struct.unpack_from('<Q', stream)
 
 
 def read_vint(stream):
@@ -35,16 +84,32 @@ def read_vint(stream):
 
     # vints have a max of 10 bytes
     for index in range(0, 10):
-        byte = read_byte(stream)
+        byte = read_u8(stream)
+        continue_on = ((byte & continue_mask) > 0)
+        if index == 9:
+            byte &= 0x01
+        else:
+            byte &= ~continue_mask
         total += (byte << (index * 7))
-        if not byte & continue_mask:
+        if not continue_on:
             break
 
     return total
 
 
 class RarInfo(object):
-    pass
+    def __init__(self):
+        pass
+
+
+class RarBlock(object):
+    def __init__(self, data_buffer, rar_version):
+        self.data_buffer = io.BytesIO(data_buffer)
+        self.process_data()
+        self.rar_version = rar_version
+
+    def process_data(self):
+        pass
 
 
 class RarFile(object):
@@ -63,11 +128,16 @@ class RarFile(object):
         self.archive_files = []
         self.archive_passworded = False
 
+        self.find_file_size()
         self.scan_for_rar_signature()
 
     def __del__(self):
         if self.fp:
             self.fp.close()
+
+    def find_file_size(self):
+        self.fp.seek(0, 2)
+        self.file_bytes = self.fp.tell()
 
     def read_at_offset(self, offest, count):
         self.fp.seek(offest)
@@ -89,5 +159,26 @@ class RarFile(object):
 
         return self.header_start
 
-    def read_headers(self):
-        pass
+    def blocks(self):
+        while self.fp.tell() < self.file_bytes:
+            block_start = self.fp.tell()
+            crc32 = read_u32(self.fp)
+            header_size = read_vint(self.fp)
+            header_type = read_vint(self.fp)
+            header_flags = self.parse_flags(read_vint(self.fp))
+
+            if 'data_area' in header_flags:
+                data_area_size = read_vint(self.fp)
+
+            if 'extra_area' in header_flags:
+                extra_area_size = read_vint(self.fp)
+
+            seek_bytes = block_start
+
+
+    def parse_flags(flags):
+        present_flags = []
+        for name, mask in HEADER_FLAG_MASKS.items():
+            if flags & mask:
+                present_flags.append(name)
+        return present_flags
